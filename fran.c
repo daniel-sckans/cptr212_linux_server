55dfd0c8d38d23d5e6c49b59a203882e82139274
Frawatson <54585254+Frawatson@users.noreply.github.com>
2020-10-21 22:40:33 -0500

    Update fran.c

--- fran.c
+++ fran.c
@@ -2,17 +2,15 @@
 #include <string.h>
 #include "server.h"
 
-void sample(FILE* socket_stream, char uri[128]) {
+void fran(FILE* socket_stream, char uri[148]) {
+    char* response_payload = "<!DOCTYPE html><html><head><title>Server</title></head><body><h1><i>About ZORO</i></h1><p>Zoro is the greatest swore man to ever grace the anime world.</p></body></html>";
 
-    printf("URI: %s\n", uri); 
-
-    char* response_payload = "<!DOCTYPE html><html><head><title>Fran server</title></head><body><style>* { background-color: #FFFF000; }</style><h1><i>Hello World</i></h1></body></html>"; 
     fprintf(socket_stream, "HTTP/1.1 200 OK"); 
     fprintf(socket_stream, "Content-length: %lu\n", strlen(response_payload)); 
     fprintf(socket_stream, "\n"); 
 
     // This is the body of our response.  
     fprintf(socket_stream, "%s", response_payload); 
-    fflush(socket_stream); 
+    fflush(socket_stream);
 
 }
